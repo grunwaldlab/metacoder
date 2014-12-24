00b9552773957674378671dcf134fc1dc4db574e
zachary-foster <zacharyfoster1989@gmail.com>
2014-12-24 14:58:44 -0800

    made polygon_coords
    A function to return to coordinates of polygon vertices

--- R/internal.R
+++ R/internal.R
@@ -274,3 +274,43 @@ map_unique <- function(input, func, ...) {
   func(unique_input, ...)[unique_mapping(input)]
 }
 
+
+
+#===================================================================================================
+#' Makes coordinates for a regualr polygon
+#' 
+#' Generates an n x 2 matrix containing x and y coordinates between 1 and 0 for the points of a 
+#' regular polygon. 
+#' 
+#' Inspired by (i.e. stolen from) https://gist.github.com/baptiste/2224724, which was
+#' itself inspired from a post by William Dunlap on r-help (10/09/09)
+#' 
+#' @param n (\code{numeric} of length 1) The number of vertices in the polygon.
+#' @param x (\code{numeric} of length 1) x coordinate of center
+#' @param y (\code{numeric} of length 1) y coordinate of center
+#' @param radius (\code{numeric} of length 1) The diameter of the circle.
+#' @param angle (\code{numeric} of length 1) Angle to rotate points around the center of the circle.
+#' 
+#' @export
+polygon_coords <- function(n = 5, x = 0, y = 0, radius = 1, angle = 0){
+  # Define function to make points for a single polygon --------------------------------------------
+  process_one <- function(n, x, y, r, a) {
+    if(n<3) stop("n must be more than 3!")
+    # Calculate imaginary coords - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    coords <- exp(seq(0, n)*2i*pi/n)
+    # Rotate around center - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    coords <- coords * exp(1i*a)
+    # Translate to x, y coords - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    coords <- data.frame(x = Re(coords), y = Im(coords))
+    # Scale to radius  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    coords <- coords * radius
+    # Offset center to given x, y coords - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    coords$x <- coords$x + x
+    coords$y <- coords$y + y
+    return(coords)
+  }
+  # Compile of the points for multiple polygons ----------------------------------------------------
+  output <- mapply(process_one, n, x, y, radius, angle, SIMPLIFY = FALSE)
+  group <- rep(seq_along(output), vapply(output, nrow, numeric(1)))
+  cbind(group = group, do.call(rbind, output))
+} 
