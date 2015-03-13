ad36d1f106f0714aca1c1426f44bbd032e590d7d
Zachary Foster <zacharyfoster1989@gmail.com>
2015-03-12 17:26:42 -0700

    ignored vignette output

--- inst/doc/plotting_value_distributions_on_taxonomies.R
+++
@@ -1,7 +0,0 @@
-## ----, echo=FALSE, warning=FALSE, message=FALSE--------------------------
-library(ape)
-library(metacoder)
-library(knitr)
-library(ggplot2)
-opts_chunk$set(dev='svg', cache = TRUE, warning = FALSE, message = FALSE, fig.show='hold')
-
