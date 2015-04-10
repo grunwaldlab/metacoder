edd5d75de296223758bc29a02ce362c151df1c5c
fosterz <zacharyfoster1989@gmail.com>
2015-04-09 18:30:00 -0700

    get_subtaxa reworked to be more intuitive and efficient

--- R/taxonomy.R
+++ R/taxonomy.R
@@ -941,31 +941,65 @@ get_supertaxa <- function(targets, taxa, parents, recursive = TRUE, simplify = F
 #' Given one or more taxa IDs and the edge list defining the taxonomy, return the taxon IDs of all 
 #' subtaxa
 #' 
-#' @param target (\code{character}) Taxon IDs for which subtaxa will be returned.
+#' @param targets (\code{character}) Taxon IDs for which subtaxa will be returned.
 #' @param taxa (\code{character}) Unique taxon IDs for every possible taxon.
-#' @param parents (\code{character}) Unique taxon IDs for the supertaxa of every possible taxon. The
+#' @param parents (\code{character}) Unique taxon IDs for the subtaxa of every possible taxon. The
 #'   lack of a parent should be coded as \code{NA}.
-#' @param recursive (\code{logical}) If \code{FALSE}, only return the subtaxa one level below the
+#' @param recursive (\code{logical}) If \code{FALSE}, only return the subtaxa one level above the
 #'   target taxa. If \code{TRUE}, return all the subtaxa of every subtaxa, etc. 
-#'   
-#' @return (\code{integer}) The IDs of subtaxa of the target taxa.
+#' @param simplify (\code{logical}) If \code{TRUE}, then combine all the results into a single
+#'   vector of unique taxon IDs 
+#' @param include_target (\code{logical}) If \code{TRUE}, the target taxa are included in the output
+#'  
+#' @return If \code{simplify = FALSE}, then a list of vectors of taxon IDs are returned 
+#'   corresponding to the \code{target} argument. If \code{simplify = TRUE}, then the unique taxon
+#'   IDs for all \code{target} taxa are returned in a single vector.
 #' 
 #' @export
-get_subtaxa <- function(target, taxa, parents, recursive = TRUE) {
+get_subtaxa <- function(targets, taxa, parents, recursive = TRUE, simplify = FALSE) {
   # Argument validataion ---------------------------------------------------------------------------
-  if (length(target) == 0) stop("Argument 'target' has 0 length")
-  if (!all(target %in% taxa)) stop("All 'target' taxon IDs not found in 'taxa' taxon IDs")
+  if (length(targets) == 0) stop("Argument 'targets' has 0 length")
+  if (!all(targets %in% taxa)) stop("All 'targets' taxon IDs not found in 'taxa' taxon IDs")
   validate_edge_list(taxa, parents)
-  # Get subtaxa ------------------------------------------------------------------------------------
-  for_one_taxa <- function(target) {
-    which(target == parents)
+  parents[!(parents %in% taxa)] <- NA
+    
+  # Get level of each input to determin processing order -------------------------------------------
+  levels <- vapply(get_supertaxa(targets, taxa, parents), length, numeric(1))
+  targets <- targets[order(levels, decreasing = TRUE)]
+
+  # Recursive function for one target --------------------------------------------------------------
+  get_one <- function(target, output) {
+    if (target %in% names(output)) { #if this taxa has already been processed
+      return(c(target, output[[target]]))
+    } else {
+      subtaxa <- taxa[parents == target]
+      subtaxa <- subtaxa[!is.na(subtaxa)]
+      if (recursive) {
+        if (length(subtaxa) == 0) {
+          return(target)
+        } else {
+          subsubtaxa <- unlist(lapply(subtaxa, get_one, output = output))
+          return(c(target, subsubtaxa))
+        }
+      } else {
+        return(subtaxa)
+      }
+    }
   }
-  subtaxa <- unlist(lapply(target, for_one_taxa))
-  if (recursive && length(subtaxa) > 0) {
-    subtaxa <- unlist(c(subtaxa, lapply(subtaxa, get_subtaxa,
-                                        taxa = taxa, parents = parents, recursive = recursive)))
-  } 
-  return(subtaxa) 
+  
+  # Apply recursive function -----------------------------------------------------------------------
+  output <- list()
+  for (target in targets)  {
+    result <- list(get_one(target, output))
+    result[[1]] <- result[[1]][-1] #remove target from start of each list
+    names(result) <- target
+    output <- c(output, result)
+  }
+  
+  # Apply `simplify` option ------------------------------------------------------------------------
+  if (simplify) output <- unique(unname(unlist(output)))
+
+  return(output)
 }
 
 
