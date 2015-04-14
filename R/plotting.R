2565db1407ae65c225ea33e7639b7303ba8b835e
fosterz <zacharyfoster1989@gmail.com>
2015-04-14 16:17:58 -0700

    new alignment plotting function idea

--- R/plotting.R
+++ R/plotting.R
@@ -853,3 +853,31 @@ new_plot_image_tree <- function(taxon_id, parent_id, size = NULL, vertex_image =
     return(the_plot)    
   }
 }
+
+
+
+#===================================================================================================
+#' Display sequence alignment
+#' 
+#' Make a plot pf a sequence alignment for an overview of alignment structure. 
+#' NOT FINISHED.
+#' 
+#' @param alignment (\code{DNAbin}) A matrix representing a sequence alignment. 
+#' 
+plot_alignment <- function(alignment) {
+  color_key <- c("A" = "#a6cee3", "T" = "#1f78b4", "C" = "#b2df8a", "G" = "#33a02c", "-" = "#DDDDDD")
+  alignment <- as.character(alignment)
+  molten_alignment <- reshape::melt.matrix(alignment)
+  names(molten_alignment) <- c("name", "position", "base")
+  molten_alignment$base <-  toupper(molten_alignment$base)
+  molten_alignment$color <- color_key[molten_alignment$base]
+  molten_alignment$color[is.na(molten_alignment$color)] <- "#FFFFFF"
+  ggplot(molten_alignment, aes(x = position, y = name)) +
+    geom_tile(fill = molten_alignment$color) +
+    theme(panel.grid = element_blank(), 
+          panel.background = element_blank(),
+          axis.title = element_blank(),
+          axis.text  =  element_blank(),
+          axis.ticks = element_blank(), 
+          axis.line  = element_blank())
+}
