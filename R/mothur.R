2e0f0831873078fc55f434ca8ee5911051971491
fosterz <zacharyfoster1989@gmail.com>
2016-05-10 12:51:36 -0700

    added draft mothur summary parsing function

--- R/mothur.R
+++ R/mothur.R
@@ -26,3 +26,48 @@ parse_summary_seqs <- function(text = NULL, file = NULL) {
   # Parse into a data.frame
   utils::read.table(text = text, sep = "\t", header = TRUE, row.names = 1)
 }
+
+
+
+
+#' Parse mothur classification summary file
+#' 
+#' Parse mothur classification summary file
+#' 
+#' @param file_path (\code{character} of length 1)
+#' The file path to the input file.
+#' 
+#' @return \code{\link{classified}}
+#' 
+#' @keywords internal
+parse_mothur_summary <- function(file_path, unclassified = FALSE) {
+  
+  # Read file
+  content <- readLines(file_path)
+  
+  # Parse header to make key
+  header <- strsplit(content[[1]], split = "\t")[[1]]
+  key <- c("taxon_info", "class", rep("taxon_info", length(header) - 2))
+  key_names <- header
+  key_names[2] <- ""
+  names(key) <- key_names
+  
+  # Make regex
+  regex <- paste0("^", paste0(collapse = "\t", rep("(.*?)", length(header))), "$")
+  
+  # Remove 'unclassified' rows
+  if (! unclassified) {
+    unclassified_rows <- grepl(content, pattern = "^(.*?)\\t(.*?)\\tunclassified\\t")
+    content <- content[! unclassified_rows]
+    message(paste0("Removed ", sum(unclassified_rows), " unclassified rows."))
+  }
+  
+  # Extract taxonomic data
+  extract_taxonomy(content[-1],
+                   key = key,
+                   regex = regex,
+                   class_key = "name",
+                   class_sep = "\\.",
+                   return_input = FALSE,
+                   return_match = FALSE)
+}
