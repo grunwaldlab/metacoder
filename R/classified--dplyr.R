9ec841700999ff94a58168c5f5834a88afaba1f7
Zachary Foster <zacharyfoster1989@gmail.com>
2016-05-26 01:28:37 -0700

    drafted filter; have not tested yet

--- R/classified--dplyr.R
+++ R/classified--dplyr.R
@@ -1,4 +1,13 @@
+#' @export
+filter <- function(.data, ...) {
+  UseMethod("filter")
+}
 
+
+#' @export
+filter.default <- function(.data, ...) {
+  dplyr::filter(.data, ...)
+}
 
 #' Filter \code{\link{classified}} on a list of conditions
 #'
@@ -22,47 +31,80 @@
 #' @return \code{\link{classified}}
 #'
 #' @export
-filter.classified <- function(obj, ..., subtaxa = TRUE, supertaxa = FALSE, itemless = TRUE, taxonless = FALSE) {
-  filter_(obj, .dots = lazyeval::lazy_dots(...))
+filter.classified <- function(obj, ...,
+                              subtaxa = TRUE, supertaxa = FALSE,
+                              itemless = TRUE, taxonless = FALSE) {
+  filter_.classified(obj, ..., .dots = lazyeval::lazy_dots(...),
+                     subtaxa = subtaxa, supertaxa = supertaxa,
+                     itemless = itemless, taxonless = taxonless)
 }
 
 
+#' Standard evaluation version of \code{\link{filter}}
+#' 
 #' @export
 #' @rdname filter
-filter_.classified <- function(obj, taxon = taxon_ids(x), item = seq_along(x$item_taxon_ids),
-                              subtaxa = TRUE, supertaxa = FALSE, itemless = TRUE, ...) {
+filter_.classified <- function(obj, ..., .dots,
+                               subtaxa = TRUE, supertaxa = FALSE,
+                               itemless = TRUE, taxonless = FALSE) {
+  var_names_in_calls <- function(.dots) {
+    components <- lapply(.dots, function(x) as.character(x$expr))
+    lapply(components,
+           function(x) x[x %in% c(colnames(obj$taxon_data), colnames(obj$item_data))])
+  }
+  
+  
   # non-standard argument evaluation
-  parsed_taxon <- lazyeval::lazy_eval(lazyeval::lazy(taxon), data = taxon_data(x))
-  parsed_item <- lazyeval::lazy_eval(lazyeval::lazy(item), data = item_data(x))
+  parsed_data <- lazyeval::lazy_eval(.dots, data = taxon_data(obj))
+  
+  # Check that taxon and item columns are not mixed
+  cols_used <- var_names_in_calls(.dots)
+  in_taxon_data <- vapply(cols_used, function(x) any(x %in% colnames(obj$taxon_data)), logical(1))
+  in_item_data <- vapply(cols_used, function(x) any(x %in% colnames(obj$item_data)), logical(1))
+  invalid_mixtures <- in_taxon_data & in_item_data
+  if (sum(invalid_mixtures) > 0) {
+    char_conditions <- vapply(.dots, function(x) deparse(x$expr), character(1))
+    invalid_list <- paste("   ", which(invalid_mixtures), ": ", char_conditions[invalid_mixtures], "\n")
+    stop(paste0(collapse = "",
+                c("The following filtering conditions are an invalid mixture of taxon_data and item_data column names:\n",
+                  invalid_list)))
+  }
+  
+  # Combine taxa filters
+  taxa_selection <- Reduce(`&`, parsed_data[in_taxon_data])
+
+  # Combine item filters
+  item_selection <- Reduce(`&`, parsed_data[in_item_data])
   
   # Get taxa of subset
-  new_taxa <- unique(c(x$taxon_ids[parsed_taxon],
+  new_taxa <- unique(c(obj$taxon_data$taxon_ids[taxa_selection],
                        if (subtaxa) {
-                         subtaxa(x, subset = parsed_taxon, simplify = TRUE)
+                         subtaxa(obj, subset = taxa_selection, simplify = TRUE)
                        },
                        if (supertaxa) {
-                         supertaxa(x, subset = parsed_taxon, simplify = TRUE, include_input = FALSE)
+                         supertaxa(obj, subset = taxa_selection, simplify = TRUE, include_input = FALSE)
                        }))
   
   # Get items of subset
-  inluded_items <- intersect(which(x$item_taxon_ids %in% new_taxa),
-                             parsed_item)
+  inluded_items <- intersect(which(obj$item_data$item_taxon_ids %in% new_taxa),
+                             item_selection)
   
-  # Make output
-  output <- classified(taxon_ids = new_taxa,
-                       parent_ids =  x$parent_ids[new_taxa],
-                       item_taxon_ids = x$item_taxon_ids[inluded_items],
-                       taxon_data = x$taxon_data[new_taxa, , drop = FALSE],
-                       item_data = x$item_data[inluded_items, , drop = FALSE],
-                       taxon_funcs = x$taxon_funcs,
-                       item_funcs = x$item_funcs)
+  # Apply selection
+  obj$taxa <- obj$taxa[new_taxa]
+  obj$taxon_data <- obj$taxon_data[obj$taxon_data$taxon_ids %in% new_taxa, , drop = FALSE]
+  obj$item_data <- obj$item_data[obj$item_data$item_taxon_ids %in% inluded_items, , drop = FALSE]
   
   # Remove taxa with no items
   if (! itemless) {
-    taxa_with_items <- item_counts(output) > 0
-    output$taxon_ids <- output$taxon_ids[taxa_with_items]
-    output$parent_ids <- output$parent_ids[taxa_with_items]
-    output$taxon_data <- output$taxon_data[taxa_with_items, , drop = FALSE]
+    taxa_with_items <- item_counts(obj) > 0
+    obj$taxa <- obj$taxa[taxa_with_items]
+    obj$taxon_data <- obj$taxon_data[obj$taxon_data$taxon_ids %in% taxa_with_items, , drop = FALSE]
   }
-  return(output)
+  
+  # Remove items with no taxa
+  if (! taxonless) {
+    obj$item_data <- obj$taxon_data[obj$item_data$item_taxon_ids %in% obj$taxon_data$taxon_ids, , drop = FALSE]
+  }
+  
+  return(obj)
 }
