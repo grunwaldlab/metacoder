87925b383676df84b442f1959c9c271b2bc54406
fosterz <zacharyfoster1989@gmail.com>
2016-05-31 09:31:18 -0700

    added select_items code and tests

--- R/classified--select.R
+++ R/classified--select.R
@@ -50,3 +50,59 @@ select_taxa <- function(.data, ...) {
   
   return(.data)
 }
+
+
+
+
+#' Subset columns in a \code{\link{classified}} object
+#' 
+#' Subsets \code{item_data} columns in a \code{\link{classified}} object.
+#' Takes and returns a \code{\link{classified}} object.
+#' Any column name that appears in \code{item_data(.data)} can be used as if it was a vector on its own.
+#' @param .data \code{\link{classified}}
+#' @param ... One or more column names to return in the new object.
+#' This can be one of three things:
+#' \describe{
+#'    \item{\code{character}}{The name of a column in \code{item_data}}
+#'    \item{\code{unquoted column name}}{The name of a column in \code{item_data} typed as if it was a varaible on its own.}
+#'    \item{\code{logical}}{A \code{TRUE}/\code{FALSE} vector of length equal to the number of columns in \code{item_data}}
+#'    \item{\code{numeric}}{Indexes of columns in \code{item_data}}
+#'  }
+#'  
+#' @return An object of type \code{\link{classified}}
+#' 
+#' @export
+select_items <- function(.data, ...) {
+  # non-standard argument evaluation ---------------------------------------------------------------
+  dots <- lazyeval::lazy_dots(...)
+  
+  process_one <- function(unevaluated) {
+    evaluated <- lazyeval::lazy_eval(unevaluated, data = .data$item_data)
+    if (length(as.character(unevaluated$expr)) == 1 && as.character(unevaluated$expr) %in% colnames(.data$item_data)) { # column name
+      return(colnames(.data$item_data) == as.character(unevaluated$expr))
+    } else if (is.logical(evaluated)) { # logical vector
+      if (length(evaluated) == ncol(.data$item_data)) {
+        return(evaluated)
+      } else {
+        stop("Incorrect number of columns implied by logical vector.")
+      }
+    } else if (is.numeric(evaluated)) { # index
+      if (all(evaluated %in% 1:ncol(.data$item_data))) {
+        return(1:ncol(.data$item_data) == evaluated)
+      } else {
+        stop("Index supplied does not correspond to a column index.")
+      }
+    } else {
+      stop("Could not parse column selector.")
+    }
+  }
+  
+  selected <- Reduce(`|`, c(lapply(dots, process_one),
+                            list(c(TRUE, rep(FALSE, ncol(.data$item_data) - 1)))))
+  
+  
+  # Select columns ---------------------------------------------------------------------------------
+  .data$item_data <- .data$item_data[ , selected, drop = FALSE]
+  
+  return(.data)
+}
--- tests/testthat/test--classified--select.R
+++ tests/testthat/test--classified--select.R
@@ -12,45 +12,93 @@ obj <- classified(taxa = c(1, 2, 3, 4, 5), parents = c(NA, 1, 2, 2, 1),
                                           other_col = 1:5,
                                           yet_another = 6:10, 
                                           stringsAsFactors = FALSE),
-                  item_data = data.frame(item_attr = LETTERS[1:10], other_item_col = 1:5,  stringsAsFactors = FALSE))
+                  item_data = data.frame(item_attr = LETTERS[1:10],
+                                         other_item_col = 1:10,
+                                         another_item_col = 11:20, 
+                                         stringsAsFactors = FALSE))
 #|
-#| ####  Selecting with unquoted column names
-test_that("Selecting with unquoted column names works", {
+#| ####  Selecting taxon_data with unquoted column names
+test_that("Selecting taxon_data with unquoted column names works", {
   result <- select_taxa(obj, name)
   expect_s3_class(result, "classified")
   expect_false("other_col" %in% colnames(result$taxon_data))
   expect_true(all(c("name", "taxon_ids", "parent_ids") %in% colnames(result$taxon_data)))
 })
 #|
-#| ####  Selecting with quoted column names
-test_that("Selecting with quoted column names works", {
+#| ####  Selecting taxon_data with quoted column names
+test_that("Selecting taxon_data with quoted column names works", {
   result <- select_taxa(obj, "name")
   expect_s3_class(result, "classified")
   expect_false("other_col" %in% colnames(result$taxon_data))
   expect_true(all(c("name", "taxon_ids", "parent_ids") %in% colnames(result$taxon_data)))
 })
 #|
-#| ####  Selecting with logical vector
-test_that("Selecting with logical vector works", {
+#| ####  Selecting taxon_data with logical vector
+test_that("Selecting taxon_data with logical vector works", {
   result <- select_taxa(obj, "name" == colnames(obj$taxon_data))
   expect_s3_class(result, "classified")
   expect_false("other_col" %in% colnames(result$taxon_data))
   expect_true(all(c("name", "taxon_ids", "parent_ids") %in% colnames(result$taxon_data)))
 })
 #|
-#| ####  Selecting with index
-test_that("Selecting with index works", {
+#| ####  Selecting taxon_data with index
+test_that("Selecting taxon_data with index works", {
   result <- select_taxa(obj, which("name" == colnames(obj$taxon_data)))
   expect_s3_class(result, "classified")
   expect_false("other_col" %in% colnames(result$taxon_data))
   expect_true(all(c("name", "taxon_ids", "parent_ids") %in% colnames(result$taxon_data)))
 })
 #|
-#| ####  Selecting with multiple types
-test_that("Selecting with multiple types works", {
+#| ####  Selecting taxon_data with multiple types
+test_that("Selecting taxon_data with multiple types works", {
   result <- select_taxa(obj, which("name" == colnames(obj$taxon_data)),
                         other_col, "taxon_ids", 2)
   expect_s3_class(result, "classified")
   expect_false("yet_another" %in% colnames(result$taxon_data))
   expect_true(all(c("name", "taxon_ids", "parent_ids", "other_col") %in% colnames(result$taxon_data)))
+})
+#|
+#|
+#| ### Select `item_data` columns
+#|
+#|
+#| ####  Selecting item_data with unquoted column names
+test_that("Selecting item_data with unquoted column names works", {
+  result <- select_items(obj, item_attr)
+  expect_s3_class(result, "classified")
+  expect_false("other_item_col" %in% colnames(result$item_data))
+  expect_true(all(c("item_attr", "item_taxon_ids") %in% colnames(result$item_data)))
+})
+#|
+#| ####  Selecting item_data with quoted column names
+test_that("Selecting item_data with quoted column names works", {
+  result <- select_items(obj, "item_attr")
+  expect_s3_class(result, "classified")
+  expect_false("other_item_col" %in% colnames(result$item_data))
+  expect_true(all(c("item_attr", "item_taxon_ids") %in% colnames(result$item_data)))
+})
+#|
+#| ####  Selecting item_data with logical vector
+test_that("Selecting item_data with logical vector works", {
+  result <- select_items(obj, "item_attr" == colnames(obj$item_data))
+  expect_s3_class(result, "classified")
+  expect_false("other_item_col" %in% colnames(result$item_data))
+  expect_true(all(c("item_attr", "item_taxon_ids") %in% colnames(result$item_data)))
+})
+#|
+#| ####  Selecting item_data with index
+test_that("Selecting item_data with index works", {
+  result <- select_items(obj, which("item_attr" == colnames(obj$item_data)))
+  expect_s3_class(result, "classified")
+  expect_false("other_item_col" %in% colnames(result$item_data))
+  expect_true(all(c("item_attr", "item_taxon_ids") %in% colnames(result$item_data)))
+})
+#|
+#| ####  Selecting item_data with multiple types
+test_that("Selecting item_data with multiple types works", {
+  result <- select_items(obj, which("item_attr" == colnames(obj$item_data)),
+                        other_item_col, "item_taxon_ids")
+  expect_s3_class(result, "classified")
+  expect_false("another_item_col" %in% colnames(result$item_data))
+  expect_true(all(c("item_attr", "item_taxon_ids", "other_item_col") %in% colnames(result$item_data)))
 })
