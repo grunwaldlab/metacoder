fe65355dc5ce2ef74eb2378eae34080be806c262
fosterz <zacharyfoster1989@gmail.com>
2016-08-08 11:47:58 -0700

    move documentation to new repo

--- inst/doc/website_source/knitr_settings.R
+++
@@ -1,5 +0,0 @@
-library(knitcitations)
-cite_options(max.names = 2)
-library(knitr)
-options(width = 120)
-opts_chunk$set(dev='png', fig.width = 7, fig.height = 7, warning = FALSE, message = FALSE, dpi = 150)
