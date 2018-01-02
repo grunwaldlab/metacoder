6dd4ceb31a6a718239ce0ab036a1c4170bac247a
Zachary S.L Foster <zacharyfoster1989@gmail.com>
2018-01-02 11:24:46 -0800

    added some helpful simple functions

---
+++ R/tools.R
@@ -0,0 +1,107 @@
+#' Revere complement sequences
+#'
+#' Make the reverse complement of one or more sequences stored as a character
+#' vector. This is a wrapper for \code{\link[seqinr]{comp}} for character
+#' vectors instead of lists of character vectors with one value per letter.
+#' IUPAC ambiguity code are handled and the upper/lower case is preserved.
+#' 
+#' @param seqs A character vector with one element per sequence.
+#' 
+#' @export
+rev_comp <- function(seqs) {
+  # Capture names to restore later
+  seq_names <- names(seqs)
+  
+  # Split into a list of vectors 
+  seq_list <- strsplit(seqs, "")
+  
+  # Reverse
+  seq_list <- lapply(seq_list, rev)
+  
+  # Get case of input so it can be restored later
+  seq_case <- lapply(seq_list, grepl, pattern = "^[[:upper:]]+$")
+  
+  # Find complement
+  seq_list <- lapply(seq_list, seqinr::comp, ambiguous = TRUE)
+  
+  # Restore case
+  seq_list <- lapply(seq_len(length(seq_list)), function(i) {
+    ifelse(seq_case[[i]], toupper(seq_list[[1]]), tolower(seq_list[[1]]))
+  })
+  
+  # Paste back together
+  result <- vapply(seq_list, FUN = paste0, FUN.VALUE = character(1), collapse = "")
+  
+  # Restore names
+  names(result) <- seq_names
+  
+  return(result)
+}
+
+
+#' Find complement of sequences
+#'
+#' Find the complement of one or more sequences stored as a character
+#' vector. This is a wrapper for \code{\link[seqinr]{comp}} for character
+#' vectors instead of lists of character vectors with one value per letter.
+#' IUPAC ambiguity code are handled and the upper/lower case is preserved.
+#' 
+#' @param seqs A character vector with one element per sequence.
+#' 
+#' @export
+complement <- function(seqs) {
+  # Capture names to restore later
+  seq_names <- names(seqs)
+  
+  # Split into a list of vectors 
+  seq_list <- strsplit(seqs, "")
+  
+  # Get case of input so it can be restored later
+  seq_case <- lapply(seq_list, grepl, pattern = "^[[:upper:]]+$")
+  
+  # Find complement
+  seq_list <- lapply(seq_list, seqinr::comp, ambiguous = TRUE)
+  
+  # Restore case
+  seq_list <- lapply(seq_len(length(seq_list)), function(i) {
+    ifelse(seq_case[[i]], toupper(seq_list[[1]]), tolower(seq_list[[1]]))
+  })
+  
+  # Paste back together
+  result <- vapply(seq_list, FUN = paste0, FUN.VALUE = character(1), collapse = "")
+  
+  # Restore names
+  names(result) <- seq_names
+  
+  return(result)
+}
+
+
+#' Reverse sequences
+#'
+#' Find the reverse of one or more sequences stored as a character
+#' vector. This is a wrapper for \code{\link{rev}} for character
+#' vectors instead of lists of character vectors with one value per letter.
+#' IUPAC ambiguity code are handled and the upper/lower case is preserved.
+#' 
+#' @param seqs A character vector with one element per sequence.
+#' 
+#' @export
+reverse <- function(seqs) {
+  # Capture names to restore later
+  seq_names <- names(seqs)
+  
+  # Split into a list of vectors 
+  seq_list <- strsplit(seqs, "")
+  
+  # Reverse
+  seq_list <- lapply(seq_list, rev)
+  
+  # Paste back together
+  result <- vapply(seq_list, FUN = paste0, FUN.VALUE = character(1), collapse = "")
+  
+  # Restore names
+  names(result) <- seq_names
+  
+  return(result)
+}
