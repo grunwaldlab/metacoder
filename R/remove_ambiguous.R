620456858f2ca0a2575d05a99dc32521cc6860b2
Zachary S.L Foster <zacharyfoster1989@gmail.com>
2018-08-17 09:11:45 -0700

    fix typo

--- R/remove_ambiguous.R
+++ R/remove_ambiguous.R
@@ -34,7 +34,7 @@ ambiguous_patterns <- function(unknown = TRUE, uncultured = TRUE,
                 paste0(name_regex, "*", "AMBIGUOUS", name_regex, "*"),
                 paste0(name_regex, "*", "unassigned", name_regex, "*"),
                 paste0(name_regex, "*", "Unassigned", name_regex, "*"),
-                paste0(name_regex, "*", "UNASSIGNED", name_regex, "*")
+                paste0(name_regex, "*", "UNASSIGNED", name_regex, "*"),
                 paste0(name_regex, "*", "possible", name_regex, "*"),
                 paste0(name_regex, "*", "Possible", name_regex, "*"),
                 paste0(name_regex, "*", "POSSIBLE", name_regex, "*")
@@ -49,7 +49,7 @@ ambiguous_patterns <- function(unknown = TRUE, uncultured = TRUE,
                 paste0(name_regex, "*", "UNCULTURED", name_regex, "*"), 
                 paste0(name_regex, "*", "candidatus", name_regex, "*"),
                 paste0(name_regex, "*", "Candidatus", name_regex, "*"),
-                paste0(name_regex, "*", "CANDIDATUS", name_regex, "*")
+                paste0(name_regex, "*", "CANDIDATUS", name_regex, "*"),
                 paste0(name_regex, "*", "metagenome", name_regex, "*"),
                 paste0(name_regex, "*", "Metagenome", name_regex, "*"),
                 paste0(name_regex, "*", "METAGENOME", name_regex, "*")
