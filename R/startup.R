ecc7da8d9341b8d32b1131a6d38b5ce07ceb5acf
zachary-foster <zacharyfoster1989@gmail.com>
2019-05-14 12:17:14 -0700

    remove shameless citation plug on startup

--- R/startup.R
+++ R/startup.R
@@ -7,13 +7,13 @@ startup_msg <- function() {
   my_version <- utils::packageVersion("metacoder")
   is_devel <- stringr::str_count(as.character(my_version), "\\.") == 3
   paste0('This is metacoder verison ', my_version, ' ',
-         ifelse(is_devel, crayon::bold("(development version)"), "(stable)"),
-         '. If you use metacoder for published research, please cite our paper:\n\n',
-         'Foster Z, Sharpton T and Grunwald N (2017). "Metacoder: An R package for',
-         ' visualization and manipulation of community taxonomic diversity data." ',
-         crayon::italic('PLOS Computational Biology'), ', ', crayon::bold('13'),
-         '(2), pp. 1-15. doi: 10.1371/journal.pcbi.1005404\n\n',
-         'Enter `citation("metacoder")` for a BibTeX entry for this citation.')
+         ifelse(is_devel, crayon::bold("(development version)"), "(stable)"))#,
+         # '. If you use metacoder for published research, please cite our paper:\n\n',
+         # 'Foster Z, Sharpton T and Grunwald N (2017). "Metacoder: An R package for',
+         # ' visualization and manipulation of community taxonomic diversity data." ',
+         # crayon::italic('PLOS Computational Biology'), ', ', crayon::bold('13'),
+         # '(2), pp. 1-15. doi: 10.1371/journal.pcbi.1005404\n\n',
+         # 'Enter `citation("metacoder")` for a BibTeX entry for this citation.')
 }
 
 
